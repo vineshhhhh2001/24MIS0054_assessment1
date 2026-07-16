adc3b4266d0113775f50ba29caf6c1912a55ebbc
vineshhhhh2001 <pkvv12345@gmail.com>
2026-07-16 02:54:04 +0000

    Update login message from teammate

--- appointment.py
+++ appointment.py
@@ -1,14 +1,2 @@
-print("Hospital Appointment Booking System")
-
-
-def patient_registration():
-    return "Patient registration module initialized"
-
-
 def login():
-    return "Login feature enabled"
-
-
-if __name__ == "__main__":
-    print(patient_registration())
-    print(login())
+    return "Login updated by teammate"
